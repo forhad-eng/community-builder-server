b4a3396de69bfc3d58a4e37ca066211558c153c6
forhad-eng <forhadu.cse@gmail.com>
2022-04-26 23:46:09 +0600

    activity api and book api created and connection established with client

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -15,10 +15,25 @@ async function run() {
     try {
         await client.connect()
         const activityCollection = client.db('community-builder').collection('activity')
+        const bookingCollection = client.db('community-builder').collection('booking')
 
         app.get('/', async (req, res) => {
             const cursor = activityCollection.find({})
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/activity/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const activity = await activityCollection.findOne(query)
+            res.send(activity)
+        })
+
+        app.post('/book', async (req, res) => {
+            const bookingDetails = req.body
+            console.log(bookingDetails)
+            const result = await bookingCollection.insertOne(bookingDetails)
             res.send(result)
         })
     } finally {
