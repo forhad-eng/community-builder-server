bcffc781ac5402d14fcdb1acae5803d26c2e6f1f
forhad-eng <forhadu.cse@gmail.com>
2022-04-27 01:02:06 +0600

    user's booked activity fetching api created

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ async function run() {
             res.send(activity)
         })
 
+        app.get('/book', async (req, res) => {
+            const email = req.query.email
+            const query = { email }
+            const cursor = bookingCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
         app.post('/book', async (req, res) => {
             const bookingDetails = req.body
             console.log(bookingDetails)
