d805bde5fecab02bd7727ac9faa9f9fa7292c613
forhad-eng <forhadu.cse@gmail.com>
2022-04-28 15:13:56 +0600

    jwt token ststem added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 const app = express()
@@ -7,6 +8,21 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqpsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
@@ -16,6 +32,13 @@ async function run() {
         await client.connect()
         const activityCollection = client.db('community-builder').collection('activity')
         const bookingCollection = client.db('community-builder').collection('booking')
+
+        //access token
+        app.post('/login', (req, res) => {
+            const email = req.body
+            const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ accessToken })
+        })
 
         //activity collection apis
         app.get('/', async (req, res) => {
@@ -38,9 +61,10 @@ async function run() {
         })
 
         //booking collection apis
-        app.get('/book', async (req, res) => {
+        app.get('/book', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
             const email = req.query.email
-            if (email) {
+            if (email === decodedEmail) {
                 const query = { email }
                 const cursor = bookingCollection.find(query)
                 const result = await cursor.toArray()
@@ -54,7 +78,6 @@ async function run() {
 
         app.post('/book', async (req, res) => {
             const bookingDetails = req.body
-            console.log(bookingDetails)
             const result = await bookingCollection.insertOne(bookingDetails)
             res.send(result)
         })
